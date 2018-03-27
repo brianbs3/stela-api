1fe903da135939e6e813e5fddd3ff8c4552a87f3
Brian Sizemore <briansb3@gmail.com>
2018-03-27 15:45:30 -0400

    getting ajax working

--- public/js/stela.js
+++ public/js/stela.js
@@ -1,14 +1,21 @@
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJyaWFuYnMzQGdtYWlsLmNvbSIsInBhc3MiOiJicyIsImlhdCI6MTUyMjE2MzU1NywiZXhwIjoxNTIyMjQ5OTU3fQ.COzzrR762McG2-5Cb_b2EveG-pSv2bYoSGeCLt31ots';
+
 function customerClick()
 {
   $.ajax({
-    type: 'POST',
+    type: 'GET',
     url: '/customers',
-    data: {},
+    dataType: 'json',
+    data: {token:token},
     success: function(data){
-      $('#stelaMain').html(data);
-      $(document).tooltip();
+      $.each(data, function(k, v) {
+        $('#stelaMain').append(`${v.firstName} ${v.lastName}<br>`);
+      });
     },
     error: function(jqXHR, textStatus, errorThrown){
+      console.log(jqXHR);
+      if(jqXHR.status === 403)
+        alert('403');
       if(jqXHR.readyState == 0)
         window.location.replace(global_site_redirect);
     }
