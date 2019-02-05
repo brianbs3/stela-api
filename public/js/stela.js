6dcc193496633197d93fa8f14d06b9c7094aefaa
bsizemore <bsizemore@apple.com>
2019-02-05 13:00:45 -0500

    working on it

--- public/js/stela.js
+++ public/js/stela.js
@@ -1,10 +1,12 @@
+
+
 var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJyaWFuYnMzQGdtYWlsLmNvbSIsInBhc3MiOiJicyIsImlhdCI6MTUyMjE2MzU1NywiZXhwIjoxNTIyMjQ5OTU3fQ.COzzrR762McG2-5Cb_b2EveG-pSv2bYoSGeCLt31ots';
 
 function customerClick()
 {
   $.ajax({
     type: 'GET',
-    url: '/customers',
+    url: '/clients',
     dataType: 'json',
     data: {token:token},
     success: function(data){
@@ -17,12 +19,17 @@ function customerClick()
       console.log(jqXHR);
       if(jqXHR.status === 403)
         alert('403');
-      if(jqXHR.readyState == 0)
+      else if(jqXHR.status === 401)
+        window.location.replace('/static/login.htm');
+      else if(jqXHR.readyState == 0)
         window.location.replace(global_site_redirect);
     }
   });
 }
+function getMyAppointments(){
+  axios.get('/clients');
 
+}
 function productClick()
 {
   $('#stelaMain').html('clicked on products');
