8adc7025dd6afaf224d98ef1e0f476e4ff8c6c1b
bsizemore <bsizemore@apple.com>
2019-02-05 17:37:43 -0500

    remove cookie stuff

--- routes/appointments.js
+++ routes/appointments.js
@@ -14,6 +14,11 @@ router.get('/', (req, res) => {
     //     m => {
     //         return (format === "CSV") ? res.csv(JSON.parse(JSON.stringify(m)), 1) : res.json(m);
     //     });
+    // Cookies that have not been signed
+    console.log('Cookies: ', req.cookies)
+
+    // Cookies that have been signed
+    console.log('Signed Cookies: ', req.signedCookies)
     return res.status(200).send({message: 'these are the appointments for you: '});
 });
 module.exports = router;
