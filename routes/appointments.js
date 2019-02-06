ac1a51e5326d21a089b19d88622b205f3a03fb95
Brian Sizemore <brianbs3@gmail.com>
2019-02-06 13:20:40 -0500

    working on mobile

--- routes/appointments.js
+++ routes/appointments.js
@@ -5,7 +5,7 @@ const knex = require('../config/knex');
 const tokenAuth = require('../middleware/tokenAuth');
 
 
-router.get('/', (req, res) => {
+router.get('/', tokenAuth, (req, res) => {
     let format = req.query.format || 'JSON';
     format = format.toUpperCase();
     console.log('client route');
@@ -14,11 +14,29 @@ router.get('/', (req, res) => {
     //     m => {
     //         return (format === "CSV") ? res.csv(JSON.parse(JSON.stringify(m)), 1) : res.json(m);
     //     });
-    // Cookies that have not been signed
-    console.log('Cookies: ', req.cookies)
-
-    // Cookies that have been signed
-    console.log('Signed Cookies: ', req.signedCookies)
-    return res.status(200).send({message: 'these are the appointments for you: '});
+      return res.status(200).send({message: 'these are the appointments for you: '});
 });
+
+router.get('/myMobileAppointments/:id', (req, res) => {
+    let format = req.query.format || 'JSON';
+    const { id } = req.params;
+    format = format.toUpperCase();
+    // const query = `SELECT clientFirstName, clientLastName,ts,appointmentType,appointmentDuration,clientID,areaCode,phonePrefix,phoneLineNumber,DATE_FORMAT(ts, "%b %D %Y") as formattedDate FROM stela.appointmentsview where stylistID=${id}`
+    var sel = knex.select()
+        .from('appointmentsview')
+        .select('clientFirstName', 'clientLastName','ts','appointmentType','appointmentDuration','clientID','areaCode','phonePrefix','phoneLineNumber')
+        .select(knex.raw('DATE_FORMAT(ts, "%b %D %Y") as formattedDate'))
+        .select(knex.raw('DATE_FORMAT(ts, "%l:%i %p") as formattedTime'))
+        .where('stylistID', id)
+
+
+    //     knex.raw(query)
+    //         .groupByRaw('formattedDate')
+        .then(
+            m => {
+                return (format === "CSV") ? res.csv(JSON.parse(JSON.stringify(m)), 1) : res.json(m);
+            });
+
+});
+
 module.exports = router;
