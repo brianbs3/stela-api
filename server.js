72f38502529dcfc6c9125eff3531eea31b4339db
bsizemore <bsizemore@apple.com>
2019-02-15 16:33:50 -0500

    set up ssl

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var csv  = require('csv-express');
 var bodyParser = require('body-parser');
 var morgan      = require('morgan');
 var jwt = require('jsonwebtoken');
+var https = require('https');
+var fs = require('fs');
 
 app.use(morgan('dev'));
 
@@ -11,6 +13,7 @@ const config = require('./config');
 app.set('superSecret', config.secret);
 
 app.get('/', function (req, res) {
+    console.log(JSON.stringify(req.headers));
     res.sendFile( __dirname + "/" + "index.htm" );
 });
 
@@ -36,9 +39,25 @@ app.use('/', auth);
 app.use('/clients', clients);
 app.use('/appointments', appointments);
 
-var server = app.listen(8088, function () {
-  var host = server.address().address
-  const port = process.env.PORT || 8088;
-   
-   console.log("Stela app listening at http://%s:%s", host, port)
+
+var options = {
+    key: fs.readFileSync( './cert/stylists.shear-inspirations.com.key' ),
+    cert: fs.readFileSync( './cert/stylists.shear-inspirations.com.cert' ),
+    requestCert: false,
+    rejectUnauthorized: false
+};
+
+var port = process.env.PORT || 8089;
+
+var server = https.createServer( options, app );
+
+server.listen( port, function () {
+    console.log( 'Express server listening on port ' + server.address().port );
 });
+
+// var server = app.listen(8088, function () {
+//   var host = server.address().address
+//   const port = process.env.PORT || 8088;
+//
+//    console.log("Stela app listening at http://%s:%s", host, port)
+// });
